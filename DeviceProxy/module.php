c0d4587fabf4c22fc83441814ea092965fd335f8
mxm73745 <martin-mertens@web.de>
2016-07-10 11:59:36 +0200

    ok

--- DeviceProxy/module.php
+++ DeviceProxy/module.php
@@ -1,18 +1,19 @@
 <?
 
-	class DeviceProxy extends IPSModule
+	class Umrechnen extends IPSModule
 	{
+		
 		public function Create()
 		{
 			//Never delete this line!
 			parent::Create();
 			
-			$this->RegisterPropertyInteger("SourceInstance", 0);
+			$this->RegisterPropertyInteger("SourceVariable", 0);
 			$this->RegisterPropertyString("Formula", "\$Value/10*sin(30)*pi()");
 			
 			$this->RegisterVariableFloat("Value", "Value", "", 0);
 		}
-
+	
 		public function ApplyChanges()
 		{
 			
@@ -20,32 +21,37 @@
 			parent::ApplyChanges();
 			
 			//Create our trigger
-			if(IPS_VariableExists($this->ReadPropertyInteger("SourceInstance"))) {
+			if(IPS_VariableExists($this->ReadPropertyInteger("SourceVariable"))) {
 				$eid = @IPS_GetObjectIDByIdent("SourceTrigger", $this->InstanceID);
 				if($eid == false) {
 					$eid = IPS_CreateEvent(0 /* Trigger */);
 					IPS_SetParent($eid, $this->InstanceID);
 					IPS_SetIdent($eid, "SourceTrigger");
-					IPS_SetName($eid, "Trigger for #".$this->ReadPropertyInteger("SourceInstance"));
+					IPS_SetName($eid, "Trigger for #".$this->ReadPropertyInteger("SourceVariable"));
 				}
-				IPS_SetEventTrigger($eid, 0, $this->ReadPropertyInteger("SourceInstance"));
-				//IPS_SetEventScript($eid, "SetValue(IPS_GetObjectIDByIdent(\"Value\", \$_IPS['TARGET']), DPR_DatapointChange(\$_IPS['TARGET'], \$_IPS['VALUE']));");
+				IPS_SetEventTrigger($eid, 0, $this->ReadPropertyInteger("SourceVariable"));
+				IPS_SetEventScript($eid, "SetValue(IPS_GetObjectIDByIdent(\"Value\", \$_IPS['TARGET']), DPR_Calculate(\$_IPS['TARGET'], \$_IPS['VALUE']));");
 				IPS_SetEventActive($eid, true);
 			}
-			else
-			{
-				echo "Keine Instanz ausgewählt."
-			}			
 			
 		}
-		
-		public function DatapointChange(float $Value)
-		{		
+	
+		/**
+		* This function will be available automatically after the module is imported with the module control.
+		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+		*
+		* UMR_Calculate($id);
+		*
+		*/
+		public function Calculate(float $Value)
+		{
+			
+			eval("\$Value = " . $this->ReadPropertyString("Formula") . ";");
 			
 			return $Value;
 		
 		}
-
+	
 	}
 
 ?>
