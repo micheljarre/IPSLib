d37333893c9d45cf0a4d3885a32467c5eb5d0625
Martin <martin-mertens@web.de>
2016-12-27 16:54:48 +0100

    m

--- CheckMK/module.php
+++ CheckMK/module.php
@@ -2,6 +2,11 @@
 
 	class CheckMK extends IPSModule {
 		
+		
+		const $CMK_URL_ALLHOSTS =  "check_mk/view.py?view_name=allhosts";  
+		const $CMK_URL_JSON 	=  "output_format=JSON";  
+		
+		
 		public function Create() {
 			//Never delete this line!
 			parent::Create();
@@ -12,15 +17,44 @@
 			$this->RegisterPropertyString("Username", "");
 			$this->RegisterPropertyString("Password", "");
 			$this->RegisterPropertyInteger("Root", 0);
+						
+			$this->RegisterPropertyInteger("DataMinutes", 1);
+			$this->RegisterPropertyInteger("ConfigHours", 12);			
+					
 			$this->RegisterPropertyBoolean("Ping", "");
-			$this->RegisterPropertyBoolean("WOL", "");
+			$this->RegisterPropertyBoolean("WOL", "");	
+			
+			$this->RegisterTimer("ConfigTimer", 0, 'CMK_UpdateConfig();');			
+			$this->RegisterTimer("UpdateTimer", 0, 'CMK_UpdateData();');						
 		}
 	
 		public function ApplyChanges() {
 			//Never delete this line!
-			parent::ApplyChanges();
-
+			parent::ApplyChanges();			
+			$this->SetTimerInterval("ConfigTimer", $this->ReadPropertyInteger("ConfigHours") * 60 * 60);
+			$this->SetTimerInterval("ConfigTimer", $this->ReadPropertyInteger("DataMinutes") * 1000 * 60);
 		}
+		
+		private function UpdateConfig()
+		{
+			IPS_LogMessage(__CLASS__, "Aktualisiere Konfigurationsdaten zu Site " . $this->ReadPropertyString("Site"));
+			
+			$url = 'http://' . $this->ReadPropertyString("User") . ':' . $this->ReadPropertyString("Password") . '@' . $this->ReadPropertyString("Server") . ':'  . $this->ReadPropertyString("Port") . '/' . $this->ReadPropertyString("Site") . '/' . $CMK_URL_ALLHOSTS . '&' . $CMK_URL_JSON;
+			
+			$config = json_decode(file_get_contents($url));
+			
+			
+			
+			
+		}
+		
+		
+		private function UpdateData()
+		{
+			IPS_LogMessage(__CLASS__, "Aktualisiere Monitoringdaten zu Site " . $this->ReadPropertyString("Site"));
+			
+			
+		}		
 		
 	}
 
